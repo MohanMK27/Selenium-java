01955d9256a462dcc4240c2043c8bc1461f73eb5
MohanMK27 <42564062+MohanMK27@users.noreply.github.com>
2019-05-24 16:17:14 +0530

    Update selenium1-open webpage.java

--- selenium1-open webpage.java
+++ selenium1-open webpage.java
@@ -10,12 +10,9 @@ public class chromemk { //change according to your class name
 public static void main(String[] args) throws InterruptedException
 {
 	
-	System.setProperty("webdriver.chrome.driver","C:\\selenium g\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver","C:\\selenium g\\chromedriver.exe"); // It is chrome driver location path
 	WebDriver driver = new ChromeDriver();
 	driver.get("https://www.gmail.com");
-	driver.quit();
-	
-	
-	
+	driver.quit();	
 }
 }
