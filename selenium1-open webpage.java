904911b6f0e4c8d6f9ea2455926212c92ef1ae88
MohanMK27 <mohangethack27@gmail.com>
2019-05-24 16:11:40 +0530

    First commit

---
+++ selenium1-open webpage.java
@@ -0,0 +1,21 @@
+package selenium1; //change according to your package name
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class chromemk { //change according to your class name
+
+public static void main(String[] args) throws InterruptedException
+{
+	
+	System.setProperty("webdriver.chrome.driver","C:\\selenium g\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.gmail.com");
+	driver.quit();
+	
+	
+	
+}
+}
